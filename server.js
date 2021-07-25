17d3292e5873af91f88ed763379707a1912b8e49
josemariadecaarvalho <josemariadecaarvalho@hotmail.com>
2021-07-25 15:02:31 -0300

    package.json

---
+++ server.js
@@ -0,0 +1,11 @@
+const http  =   require("http");
+const host =  "127.0.0.1";
+const port = 3000 ;
+const server = http.createServer((req,res) => {
+    res.statusCode = 200 ;
+    res.setHeader("Content-Type","text-plain");
+    res.end("Olá mundo  \n Meu primeiro script");
+});
+server.listen(port,host,()=> {
+    console.log(`server running at http://${host}:${port}`);
+});
